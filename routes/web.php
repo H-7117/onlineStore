597344ee031737f5d8eb7aaf67c6a57afcadf4e1
hamood <hamood_alattas@gmail.com>
2023-12-20 19:36:53 +0300

    add acount and muaccountContoller

--- routes/web.php
+++ routes/web.php
@@ -38,7 +38,9 @@ Route::get('/cart/delete', 'App\Http\Controllers\CartController@delete')->name("
 Route::post('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
 Route::middleware('auth')->group(function () {
 Route::get('/cart/purchase', 'App\Http\Controllers\CartController@purchase')->name("cart.purchase");
+Route::get('/my-account/orders', 'App\Http\Controllers\MyAccountController@orders')->name("myaccount.orders");
 });
-    
+
+
 Auth::routes();
 
